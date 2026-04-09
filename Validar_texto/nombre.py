d97110f30c86c6139102f3da6694360829bd5816
auxiliariso <aux.iso@Forvac.com.mx>
2026-04-09 13:15:26 -0600

    Proyecto 2 de repaso-validar_nombre

---
+++ Validar_texto/nombre.py
@@ -0,0 +1,9 @@
+nombre = "Auxiliar ISO"
+
+validar_nombre = str(input("Ingrese el nombre porfavor: "))
+
+if nombre == validar_nombre:
+    print("Es correcto: ")
+
+else: 
+    print("Nombre incorrecto")
